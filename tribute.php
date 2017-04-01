2911fb521511fc98a6e11823293ce59f52bc26f4
Justin <jstnwrds55@gmail.com>
2017-04-01 04:00:19 -0600

    first commit

---
+++ tribute.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Damien Walters</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    
+        <h1 class="text-center">Damien Walters</h1>
+        <h3 class="text-center">One of the greatest stuntmen alive</h3>
+        <div class="thumbnail">
+          <img class="img-rounded" src="damien.jpeg"><p class="text-center">Damien Walters lookin' all thug</p></img>
+      </div>
+    
+      <div class="film-timeline">
+        <h4 class="text-center">Damien Walters has starred in many popular movies working in stunts such as:</h4>
+        <ol class="film-list text-center list-group">
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt1186367/" target="_blank">Ninja Assassin</a> - 2009</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt1250777/" target="_blank">Kick-Ass</a> - 2010</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt0446029/" target="_blank">Scott Pilgrim vs. the World</a> - 2010</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt1034389/" target="_blank">The Eagle</a> - 2011</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt1464540/" target="_blank">I Am Number Four</a> - 2011</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt1297919/" target="_blank">Blitz</a> - 2011</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt0458339/" target="_blank">Captain America: The First Avenger</a> - 2011</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt1515091/" target="_blank">Sherlock Holmes: A Game of Shadows</a> - 2011</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt1074638/" target="_blank">Skyfall</a> - 2012</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt1335975/" target="_blank">47 Ronin</a> - 2013</li>
+          
+          <li class="list-group-item"><a href="http://www.imdb.com/title/tt2802144/" target="_blank">Kingsman: The Secret Service</a> - 2015</li>
+          
+          <li class="list-group-item"><h5>In 2010, he won the Taurus World Stunt Awards for "Best Fight" in the film Ninja Assassin.</h5></li>
+        </ol>
+      </div>
+    
+      <div class="award-timeline">
+        <h4 class="text-center">Walters also participated in four Trampoline World Championships and placed the following:</h4>
+        <ul class=" award-list list-group text-center">
+          
+          <li>In 2003, he was one of four members of the British team winning the World Title in the team competition</li>
+          
+          <li>In the years 2001 and 2007, he ranked 4th with the team.</li>
+          
+          <li>He finished 5th in singles competition in 2003 and 2005.</li>
+          
+        </ul>
+        <p>Walters continues to pursue his passion for gymnastics through Freerunning where he mashes together his gymnastics and stuntman abilities. On YouTube, he is one of the most watched freerunners, with his videos having a total of over 115 million views. Currently he is running his own gym, Derby City Gymnastics Club, in Derby and shooting films. You can visit his <a href="https://www.youtube.com/user/damienwalters" target="_blank">Youtube Channel</a> or <a href="https://www.facebook.com/Damien-Walters-official--283351168355035/" target="_blank">Facebook Page</a> to see more of his work or head over to his <a href="http://www.imdb.com/name/nm3215480/" target="_blank">IMDB</a> page to see more of his previous and upcoming film credits.<p>
+        
+    
+    </body>
+</html>
